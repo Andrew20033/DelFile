276d107c4a7f19f4960a00a07ca1d6fb1d3ee1f2
Stuk_uc23@student.itstep.org <Stuk_uc23@student.itstep.org>
2022-10-28 18:43:29 +0300

    Добавьте файлы проекта.

---
+++ DelFile/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string appRoot = AppDomain.CurrentDomain.BaseDirectory;
+            if (!appRoot.EndsWith("\\")) appRoot += "\\";
+            SortedSet<string> namesToIgnore = new SortedSet<string>(StringComparer.OrdinalIgnoreCase) {
+    "1.txt",
+    "Launcher.exe",
+    "MasterList"
+};
+            DirectoryInfo appRootDir = new DirectoryInfo(appRoot);
+            foreach (FileSystemInfo info in appRootDir.EnumerateFileSystemInfos(appRoot, "*.*", SearchOption.AllDirectories))
+            {
+                string name = info.FullName.Substring(appRoot.Length);
+                if (!namesToIgnore.Contains(name))
+                {
+                    info.Delete();
+                }
+            }
+        }
+    }
+}
